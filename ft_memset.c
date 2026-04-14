bde402797ed7b0ee2127bcf3926818eafbd9f3f9
André Amador <andreamador@MacBook-Pro.local>
2026-04-14 14:23:48 +0100

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,33 @@
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 12:51:16 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/04/14 12:51:17 by apinho-a         ###   ########.fr       */
+/*   Updated: 2026/04/14 14:23:36 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+
+void	*memset(void *b, int c, size_t len)
+{
+	unsigned char	c_char;
+	unsigned char	*b_str;
+
+	c_char = (unsigned char) c;
+	b_str = (unsigned char *) b;
+	while (len--)
+		*b_str++ = c_char;
+	return (b);
+}
+
+/* #include <unistd.h>
+int	main()
+{
+	unsigned char b_str[18] = "xxxxxxxxxxxxxxxxx";
+	int	i;
+	
+	memset(b_str, 121, 5);
+	i = 0;
+	while(b_str[i++] != 0)
+		write(1,&b_str[i],1);
+	return(0);
+} */
