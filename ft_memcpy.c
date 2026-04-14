9623af6f6947e3ea2f1c503c9427bc40a7de1b68
André Amador <andreamador@MacBook-Pro.local>
2026-04-14 18:48:27 +0100

    memmove

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,36 +6,36 @@
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 14:54:33 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/04/14 17:22:26 by apinho-a         ###   ########.fr       */
+/*   Updated: 2026/04/14 18:03:10 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	index;
 
 	index = 0;
 	while (index < n)
 	{
-		*((unsigned char *) dst + index) = *((unsigned char *) src + index);
+		*((unsigned char *) dest + index) = *((unsigned char *) src + index);
 		index++;
 	}
-	return (dst);
+	return (dest);
 }
 
 /* #include <unistd.h>
 int	main()
 {
 	char *src ="abcdefghij";
-	unsigned char dst[11] = "0123456789";
+	unsigned char dest[11] = "0123456789";
 	unsigned int i;
 
-	ft_memcpy(dst, src, 3);
+	ft_memcpy(dest, src, 3);
 
 	i = 0;
-	while(dst[i])
-		write(1, &dst[i++], 1);
+	while(dest[i])
+		write(1, &dest[i++], 1);
 	return (0);
 } */
---
+++ ft_memmove.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/14 17:23:45 by apinho-a          #+#    #+#             */
+/*   Updated: 2026/04/14 18:48:15 by apinho-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t count;
+	
+	if (dest > src)
+	{
+		while (n > 0)
+		{
+			n--;			
+			*((unsigned char *) dest + n) = *((unsigned char *) src + n);
+		}
+	}
+	else
+	{
+		count = 0;
+		while (count < n)
+		{
+			*((unsigned char *) dest + count) = *((unsigned char *) src + count);
+			count++;
+		}
+	}
+	return (dest);
+}
+
+#include <unistd.h>
+int main()
+{
+	unsigned char src[14] = "Learningisfun";
+	size_t	index;
+
+	ft_memmove(src + 8, src, 2);
+
+	index = 0;
+	while (src[index])
+		write(1, &src[index++], 1);
+	return (0);
+}
