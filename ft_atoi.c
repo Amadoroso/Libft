c7b141ab21b6fe8649dc68e68c1149c7b386ecdf
André Amador <andreamador@MacBook-Pro.local>
2026-04-20 18:31:31 +0100

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 16:44:37 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/04/20 18:16:32 by apinho-a         ###   ########.fr       */
+/*   Updated: 2026/04/20 18:31:19 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,11 @@ int	ft_atoi(const char *nptr)
 	return (result);
 }
 
-#include <stdio.h>
+/* #include <stdio.h>
 #include <stdlib.h>
 int	main()
 {
 	printf("%d\n", ft_atoi("	-1x234a"));
 	printf("%d\n", atoi("	-1x234a"));
 	return (0);
-}
+} */
